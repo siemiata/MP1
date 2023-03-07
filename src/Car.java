5c79315e1603a92b232b0204bbec10cbabf5fa1d
Aleksander Siemiątkowski <s22201@pjwstk.edu.pl>
2023-03-07 22:56:37 +0100

    second try

---
+++ src/Car.java
@@ -0,0 +1,66 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Car {
+    private String brand;
+    private String color;
+    int wheelsSize;
+
+    public Car(String brand, String color, int wheelsSize) {
+        this.brand = brand;
+        this.color = color;
+        this.wheelsSize = wheelsSize;
+        addCar(this);
+    }
+
+    @Override
+    public String toString() {
+        return "Car: " + brand + ", id: " + super.toString();
+    }
+
+    private static List<Car> extent = new ArrayList<>();
+
+    private static void addCar(Car car){
+        extent.add(car);
+    }
+
+    private static void removeCar(Car car){
+        extent.remove(car);
+    }
+
+    public static void showExtent(){
+        System.out.println("Extent of the class: " + Car.class.getName());
+
+        for(Car car : extent){
+            System.out.println(car);
+        }
+    }
+    public static void writer() {
+        try {
+            FileWriter fw = new FileWriter("cars.txt");
+            for(Car car : extent){
+                fw.write(car.brand + " " + car.color + " " + car.wheelsSize + System.lineSeparator());
+            }
+            fw.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    public static void reader() {
+        try {
+            Scanner s = new Scanner(new File("cars.txt"));
+            while (s.hasNext()){
+                String d1 = s.next();
+                String d2 = s.next();
+                String d3 = s.next();
+                Car c = new Car(d1,d2,Integer.parseInt(d3));
+            }
+            s.close();
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Test");
+        //var car1 = new Car("Audi", "Black", 17);
+       // var car2 = new Car("Volvo", "Red", 16);
+        //var car3 = new Car("BMW", "White", 19);
+        Car.reader();
+        Car.showExtent();
+       // Car.writer();
 
-        System.out.println("Hello world!");
+
+
     }
 }
